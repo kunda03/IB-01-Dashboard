0ab25fe90500aa5356ae4118bec1a5cd066b70f0
kalpeshingale14 <104881248+kalpeshingale14@users.noreply.github.com>
2022-06-16 19:25:04 +0530

    update profile form added
    pop -up add

--- Dashboard/src/app/app-routing.module.ts
+++ Dashboard/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import{DashboardComponent} from './components/dashboard/dashboard.component';
 import { LogInComponent } from './components/log-in/log-in.component';
+import { UpdateProfileComponent } from './components/update-profile/update-profile.component';
 
 const routes: Routes = [
 
   {path:'dashboard',component:DashboardComponent},
-  { path:'log-in',component:LogInComponent}
+  { path:'log-in',component:LogInComponent},
+  { path:'update-profile',component:UpdateProfileComponent}
 ];
 
 @NgModule({
