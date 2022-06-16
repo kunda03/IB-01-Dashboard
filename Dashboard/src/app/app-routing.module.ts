bd93931fdb1f2ebe3d3ae5adb3c58a2218900444
kalpeshingale14 <104881248+kalpeshingale14@users.noreply.github.com>
2022-06-16 19:54:58 +0530

    routing added
    for components

--- Dashboard/src/app/app-routing.module.ts
+++ Dashboard/src/app/app-routing.module.ts
@@ -3,12 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 import{DashboardComponent} from './components/dashboard/dashboard.component';
 import { LogInComponent } from './components/log-in/log-in.component';
 import { UpdateProfileComponent } from './components/update-profile/update-profile.component';
+import { PpcComponent } from './components/ppc/ppc.component';
+import { LogicBuildingComponent } from './components/logic-building/logic-building.component';
+import { InternshipComponent } from './components/internship/internship.component';
+import { RawdataComponent } from './components/rawdata/rawdata.component';
 
 const routes: Routes = [
 
   {path:'dashboard',component:DashboardComponent},
   { path:'log-in',component:LogInComponent},
-  { path:'update-profile',component:UpdateProfileComponent}
+  { path:'update-profile',component:UpdateProfileComponent}, {path:'logicBuilding' ,component:LogicBuildingComponent},
+  {path:'ppc',component:PpcComponent},
+  {path:'internship',component:InternshipComponent },
+  {path:'rawdata',component:RawdataComponent}
 ];
 
 @NgModule({
